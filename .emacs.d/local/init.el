b6cf10843f6a74031bf6301284581df8dda83381
Arkady Rost <arkrost@gmail.com>
2014-04-24 03:42:48 +0400

    Added emacs window bindings

--- .emacs.d/local/init.el
+++ .emacs.d/local/init.el
@@ -23,9 +23,9 @@
   (interactive)
   (text-scale-set 0))
 
-(global-set-key (kbd "s-=") 'text-scale-increase)
-(global-set-key (kbd "s--") 'text-scale-decrease)
-(global-set-key (kbd "s-)") 'text-scale-reset)
+(global-set-key (kbd "s-f =") 'text-scale-increase)
+(global-set-key (kbd "s-f -") 'text-scale-decrease)
+(global-set-key (kbd "s-f 0") 'text-scale-reset)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Bars
@@ -34,9 +34,29 @@
 (scroll-bar-mode 0)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Theme
+;; Look and feel
+
 (load-theme 'wombat t)
+(set-default-font "Inconsolata-14")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Font
-(set-default-font "Inconsolata-14")
+;; Presets
+(setq make-backup-files nil) ; prevent backup creation
+(global-linum-mode 1) ; show line number
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Windows
+(global-set-key (kbd "s-<up>") 'windmove-up)
+(global-set-key (kbd "s-<down>") 'windmove-down)
+(global-set-key (kbd "s-<left>") 'windmove-left)
+(global-set-key (kbd "s-<right>") 'windmove-right)
+(global-set-key (kbd "s-<tab>") 'other-window)
+
+(global-set-key (kbd "s-=") 'enlarge-window)
+(global-set-key (kbd "s--") 'shrink-window)
+(global-set-key (kbd "s-+") 'enlarge-window-horizontally)
+(global-set-key (kbd "s-_") 'shrink-window-horizontally)
+(global-set-key (kbd "s-b") 'balance-windows)
+
+(global-set-key (kbd "s-w") 'delete-window)
+(global-set-key (kbd "s-o") 'delete-other-windows)
