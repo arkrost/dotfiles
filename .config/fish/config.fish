d9ab3df7d7ca24c959bfffe3b9b766ed1170503a
Arkady Rost <arkrost@gmail.com>
2015-07-15 02:27:45 +0300

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch brown
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+  set_color purple; echo -n $USER
+  set_color normal; printf ' at '
+  set_color brown; echo -n (hostname)
+  set_color normal; echo -n ' in '
+  set_color green; echo -n (prompt_pwd)
+  set_color normal; echo -n (__fish_git_prompt)
+  set_color normal; echo -n ' # '
+end
+
+function fish_right_prompt
+  set_color normal; printf '[%s]' (date +%H:%M:%S)
+end
