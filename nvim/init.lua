efce3644f3e25c7777f0659b6f0ad59d2c9dfe15
Arkady Rost <1239844+arkrost@users.noreply.github.com>
2023-11-08 02:01:07 +0000

    Refresh nvim configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,8 +47,6 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- do not yank
 vim.keymap.set({'n', 'v'}, 'x', '"_x')
-vim.keymap.set({'n', 'v'}, '<leader>d', '"_d')
-vim.keymap.set('x', '<leader>p', '"_dP')
 
 -- save position
 vim.keymap.set('n', 'J', 'mzJ`z')
@@ -62,12 +60,6 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 -- move lines
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
--- exit i mode
-vim.keymap.set('i', '<C-c>', '<Esc>', { desc = 'Exit insert mode' })
-
--- replace
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- tabs
 vim.keymap.set('n', 'te', ':tabedit<CR>', { silent = true})
@@ -97,11 +89,14 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup(
   {
     {
-      'navarasu/onedark.nvim',
+      'ellisonleao/gruvbox.nvim',
       lazy = false,
       priority = 1000,
+      opts = {
+        contrast = 'hard'
+      },
       init = function ()
-        vim.cmd('colorscheme onedark')
+        vim.cmd('colorscheme gruvbox')
       end
     },
     {
@@ -231,10 +226,6 @@ require('lazy').setup(
       end,
     },
     {
-      'tpope/vim-fugitive',
-      cmd = 'G'
-    },
-    {
       'lukas-reineke/indent-blankline.nvim',
       main = 'ibl',
       opts = {
@@ -309,81 +300,51 @@ require('lazy').setup(
     {
       'neovim/nvim-lspconfig',
       dependencies = {
-        { 'folke/neodev.nvim', config = true },
-        { 'williamboman/mason.nvim', config = true, cmd = 'Mason' },
-        'williamboman/mason-lspconfig.nvim',
         'nvim-telescope/telescope.nvim', -- see on_attach keys
         { 'hrsh7th/cmp-nvim-lsp', dependencies = { 'hrsh7th/nvim-cmp' } }
       },
       -- event = { 'BufReadPre', 'BufNewFile' },
-      ft = { 'lua', 'rust' },
+      ft = { 'rust' },
       config = function()
-        local servers = {
-          -- pyright = {},
-          rust_analyzer = {},
-          -- tsserver = {},
-          -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
-          lua_ls = {
-            Lua = {
-              workspace = { checkThirdParty = false },
-              telemetry = { enable = false },
-            },
+        local lspconfig = require('lspconfig')
+        lspconfig.rust_analyzer.setup {
+          settings = {
+            ['rust-analyzer'] = {},
           },
         }
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+          callback = function(ev)
+            local nmap = function(keys, func, desc)
+              vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = desc })
+            end
 
-        local on_attach = function(_, bufnr)
-          local nmap = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+            nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+            nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+            nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+            -- See `:help K` for why this keymap
+            nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+            nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+            -- Create a command `:Format` local to the LSP buffer
+            vim.api.nvim_buf_create_user_command(ev.buf, 'Format', function(_)
+              vim.lsp.buf.format()
+            end, { desc = 'Format current buffer with LSP' })
+
           end
+        })
 
-          nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-          nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-          nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-          nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-          nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-          -- See `:help K` for why this keymap
-          nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-          nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-          -- Lesser used LSP functionality
-          nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-          nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-          nmap('<leader>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, '[W]orkspace [L]ist Folders')
-
-          -- Create a command `:Format` local to the LSP buffer
-          vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-            vim.lsp.buf.format()
-          end, { desc = 'Format current buffer with LSP' })
-        end
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-        local mason_lspconfig = require('mason-lspconfig')
-
-        mason_lspconfig.setup {
-          ensure_installed = vim.tbl_keys(servers),
-        }
-
-        mason_lspconfig.setup_handlers {
-          function(server_name)
-            require('lspconfig')[server_name].setup {
-              capabilities = capabilities,
-              on_attach = on_attach,
-              settings = servers[server_name],
-              filetypes = (servers[server_name] or {}).filetypes,
-            }
-          end
-        }
       end
     },
   },
