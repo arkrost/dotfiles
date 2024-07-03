a15373be80b26c0c3d9d5c562bb36e239f0e09f8
Arkady Rost <1239844+arkrost@users.noreply.github.com>
2024-07-03 22:04:30 +0100

    Added LuaSnip jump next and prev keymapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -559,8 +559,24 @@ require('lazy').setup(
             }),
           },
           mapping = cmp.mapping.preset.insert {
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-n>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+            ['<C-p>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
             ['<C-u>'] = cmp.mapping.scroll_docs(-4),
             ['<C-d>'] = cmp.mapping.scroll_docs(4),
             ['<C-c>'] = cmp.mapping.close(),
