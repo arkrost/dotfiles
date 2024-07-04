93153d7dc5d2083776ed3de648e6fbac25be93f4
Arkady Rost <1239844+arkrost@users.noreply.github.com>
2024-07-05 00:12:46 +0100

    Change <C-n> -> <C-j> and <C-p> -> <C-k>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -559,7 +559,7 @@ require('lazy').setup(
             }),
           },
           mapping = cmp.mapping.preset.insert {
-            ['<C-n>'] = cmp.mapping(function(fallback)
+            ['<C-j>'] = cmp.mapping(function(fallback)
               if cmp.visible() then
                 cmp.select_next_item()
               elseif luasnip.locally_jumpable(1) then
@@ -568,7 +568,7 @@ require('lazy').setup(
                 fallback()
               end
             end, { 'i', 's' }),
-            ['<C-p>'] = cmp.mapping(function(fallback)
+            ['<C-k>'] = cmp.mapping(function(fallback)
               if cmp.visible() then
                 cmp.select_prev_item()
               elseif luasnip.locally_jumpable(-1) then
