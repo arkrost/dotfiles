8363c3e8b5caed3987623c7a925e62396805783e
Arkady Rost <1239844+arkrost@users.noreply.github.com>
2025-03-21 10:20:14 +0000

    Set BAT_THEME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,6 +28,7 @@ set -x FZF_DEFAULT_OPTS " \
     --preview 'bat --style=numbers --color=always --line-range :500 {}'
     --color fg:188,bg:-1,hl:103,fg+:222,bg+:234,hl+:104 \
     --color info:183,prompt:110,spinner:107,pointer:167,marker:215"
+set -x BAT_THEME base16
 
 alias gbc='git br | grep -v "*" | xargs git br -D'
 alias up='brew update && brew upgrade && brew cleanup'
